79426eb6736ee435050c85701085eb30699097ab
OsP93 <98236861+OsP93@users.noreply.github.com>
2023-02-07 13:50:20 +0200

    делегування корзини

--- product_scripts.js
+++ product_scripts.js
@@ -55,12 +55,21 @@ fetch(endPoint)
         basketPils.forEach((basketPil) => basketPil.addEventListener('click', function () { // Прослуховувач на кожну кнопку "корзина"
             console.log('Basket button is clicked!');
             console.log(json[basketPil.dataset.id]['c'][1]['v']);
-            // Вибираємо елемент корзину якого був клік (делегування)
-            productlist.onclick = function (event) {
+            // Вибираємо елемент на корзину якого був клік (делегування)
+            /*productlist.onclick = function (event) {
                 let target = event.target; // де був клік?
                 //console.log(target.classList);
                 if (target.classList != 'product-buttons__basket') return; // Якщо клік був не на кнопці "корзина", то ігноруємо
                 changeButton(target); // Якщо клік був на кнопці "корзина", то змінюємо іконку
+                function changeButton(arg){
+                    
+                }
+            }*/
+            productlist.onclick = function(event){
+                let target = event.target.closest('product-buttons__basket'); // Визначаємо де був клік і шукаємо найблищого предка об'єкта де був клік згідно запиту ('product-buttons__basket').
+                if (!product-buttons__basket)return;
+                console.log("YES!!!")
+
             }
 
 
